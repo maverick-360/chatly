68227ab736190443a1a5e82f776d7e4585feb2f4
Soumavo Dey <sd2337@it.jgec.ac.in>
2022-12-31 16:57:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatly-six.vercel.app",
     credentials: true,
   },
 });
